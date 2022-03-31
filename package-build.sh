2ccc010079d870c742b2dd703005371e905fff8b
Brandon Istenes <bistenes@gmail.com>
2022-03-31 07:12:23 -0700

    Ensure that script exits non-zero when a command fails (#1)

--- package-build.sh
+++ package-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 cd ses-cor-debian-base
 
+set -Eeu
+
 ARTIFACTS=/opt/bamboo5.9/artifacts
 INTEGRATION_SHARED_DIR=${ARTIFACTS}/plan-569835545/shared
 LATEST_BUILD_DIR=`ls -t ${INTEGRATION_SHARED_DIR}/|head -1`
